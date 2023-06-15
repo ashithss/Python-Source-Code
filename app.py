6d7f060682bb41fbccf40047209be40e556a53af
EC2 Default User <ec2-user@ip-172-31-33-141.ap-south-1.compute.internal>
2023-06-15 09:58:20 +0000

    I have changed message and committed 2nd time

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>I have deployed given application by using ARGOCD</h1>'
+    return '<h1>I have deployed given application by using ARGOCD at 3:57</h1>'
